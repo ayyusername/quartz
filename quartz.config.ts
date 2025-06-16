0605e97a55fe3651a497eb6811a2383864387b5b
Joshua Kadrich <j.kadrich@gmail.com>
2025-06-16 10:41:42 -0400

    Set baseUrl to empty for clean deployment

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz-topaz.vercel.app",
+    baseUrl: "",
     ignorePatterns: ["private", "templates", ".obsidian", "**/*.png", "**/*.jpg", "**/*.jpeg", "**/*.pdf", "**/Pasted*", "**/IMG_*", "**/Screenshot*"],
     defaultDateType: "created",
     generateSocialImages: false,
