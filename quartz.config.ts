568df32b5b0deb6568569b0790099d59ed92a716
Joshua Kadrich <j.kadrich@gmail.com>
2025-06-16 10:32:59 -0400

    Set baseUrl for Vercel deployment

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "",
+    baseUrl: "quartz-topaz.vercel.app",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
